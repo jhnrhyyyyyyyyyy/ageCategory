92c2fe4d85d802accc0edc2a204204d3ab97fb31
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-11-04 18:43:05 +0800

    Age Category

---
+++ ageCategory.js
@@ -0,0 +1,15 @@
+let age = "21";
+
+if (age < 5) {
+    console.log("Toddler.");
+} else if (age <= 12) {
+    console.log("Child.");
+} else if (age <= 19) {
+    console.log("Teenager.");
+} else if (age <= 35) {
+    console.log("Young Adult.");
+} else if (age <= 60) {
+    console.log("Middle-Aged.");
+} else {
+    console.log("Senior.");
+}
